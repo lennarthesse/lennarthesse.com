7723ea4f82bb087055fdf72965064dfb145b7b16
Lennart Hesse <hesse@uni-bremen.de>
2022-07-20 19:13:09 +0200

    Added mediocre search implementation

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,66 @@
-// Fade in on scroll
-/*$(window).on("load", function() {
-    $(window).scroll(function() {
-        var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-        $(".fade").each(function() {
-            var objectOffset = $(this).offset().top + 50;
+// VARIABLES //
+var topButton = document.getElementById("topBtn");              // Reference to scroll to top button
+var downIcon = document.getElementById("scroll-down-icon");     // Reference to scroll down hint
+var xPos = downIcon.style.bottom;                               // xPos of scroll down hint
+var up;                                                         // Hint moving up or not?
 
-            if (objectOffset < windowBottom) {
-                if ($(this).css("opacity")==0) {
-                    $(this).fadeTo(500, 1);
-                }
-            }
-        });
-    }).scroll();
-});*/
+var thresholdTop = $(window).innerHeight();                     // When to show scroll to top button
+var thresholdDown = 150;                                        // When to hide scroll down hint
 
-// Read more function
+var loadMoreButton = document.getElementById("js-load-more");   // Reference to load more button
+const additionalArticles = 2;                                   // Number of additional articles to be loaded
+
+var articles = $(document).find("article");                     // Collection of all articles
+var input = document.getElementById("js-search-bar");           // Reference to search input
+
+// INITIAL SETUP //
+hideArticles();                 // First hide all articles
+loadMore();                     // Then load first articles
+
+// MAIN LOOP //
+setInterval(animateHint, 75);   // Animate scroll down hint
+window.onscroll = function() {showOnScrollDown(thresholdTop, topButton), hideOnScrollDown(thresholdDown, downIcon)}; // Check if buttons should be shown or hidden
+
+// INTERNAL FUNCTIONS //
+function hideArticles() {
+    var articles = $(document).find("article");
+    for (var i = 0; i < articles.length; i++) {
+        articles[i].style.display = "none";
+    }
+}
+
+function animateHint() {
+    if (xPos == 10) {
+        up = false;
+    } else if (xPos == 0) {
+        up = true;
+    }
+    if (up) {
+        xPos++;
+    } else if (!up) {
+        xPos--;
+    }
+    downIcon.style.bottom = xPos + "px";
+}
+
+function showOnScrollDown(threshold, element) {
+    if (document.body.scrollTop > threshold || document.documentElement.scrollTop > threshold) {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }
+}
+
+function hideOnScrollDown(threshold, element) {
+    if (document.body.scrollTop > threshold || document.documentElement.scrollTop > threshold) {
+        element.style.display = "none";
+    } else {
+        element.style.display = "block";
+    }
+}
+
+
+// EXTERNAL FUNCTIONS //
 function readMore(btn) {
     var parent = btn.parentElement;
     var panel = $(parent).find(".panel")[0];
@@ -26,6 +72,53 @@ function readMore(btn) {
         panel.style.maxHeight = panel.scrollHeight + "px";
         btn.innerHTML = "Read less";
     }
+}
+
+function loadMore() {
+    var hiddenArticles = $(document).find("article:hidden");
+    var i = 0;
+
+    if (i + additionalArticles >= hiddenArticles.length) {
+        loadMoreButton.style.visibility = "hidden";
+    }
+
+    while (i < hiddenArticles.length && i < additionalArticles) {
+        hiddenArticles[i].style.display = "block";
+        i++;
+    }
+}
+
+// Search Articles 
+function searchArticles() {
+    var searchTerm = input.value.toLowerCase();
+    var matches = [];
+    // If search term is empty, reset articles and show load more
+    if (searchTerm == "") {
+        hideArticles();
+        loadMore();
+        articles[0].scrollIntoView();
+        loadMoreButton.style.visibility = "visible";
+        return;
+    }
+
+    // Loop through all articles
+    for (var i = 0; i < articles.length; i++) {
+        // If an article contains the exact search term, add it to results
+        if (articles[i].innerText.toLowerCase().search(searchTerm) > -1) {
+            matches.push(articles[i]);
+        }
+    }
+    // Only display the matches and scroll to the first one, hide load more button
+    hideArticles();
+    for (var i = 0; i < matches.length; i++) {
+        matches[i].style.display = "block";
+    }
+    if (matches[0]) {
+        matches[0].scrollIntoView();
+    }
+    loadMoreButton.style.visibility = "hidden";
+
+    // Add notifier if no results?
 }
 
 // Show side nav on burger click
@@ -47,70 +140,3 @@ function scrollToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
-
-function showOnScrollDown(threshold, element) {
-    if (document.body.scrollTop > threshold || document.documentElement.scrollTop > threshold) {
-        element.style.display = "block";
-    } else {
-        element.style.display = "none";
-    }
-}
-
-function hideOnScrollDown(threshold, element) {
-    if (document.body.scrollTop > threshold || document.documentElement.scrollTop > threshold) {
-        element.style.display = "none";
-    } else {
-        element.style.display = "block";
-    }
-}
-
-// Show scroll to top button when scrolling down and hide scroll down hint
-var topButton = document.getElementById("topBtn");
-var downIcon = document.getElementById("scroll-down-icon");
-
-var thresholdTop = $(window).innerHeight();
-var thresholdDown = 150;
-
-window.onscroll = function() {showOnScrollDown(thresholdTop, topButton), hideOnScrollDown(thresholdDown, downIcon)};
-
-// Animate scroll down hint
-var id = setInterval(frame, 75);
-var xPos = downIcon.style.bottom;
-var up;
-
-function frame() {
-    if (xPos == 10) {
-        up = false;
-    } else if (xPos == 0) {
-        up = true;
-    }
-    if (up) {
-        xPos++;
-    } else if (!up) {
-        xPos--;
-    }
-    downIcon.style.bottom = xPos + "px";
-}
-
-// Load more articles
-var loadMoreButton = document.getElementById("js-load-more");
-var additionalArticles = 2;
-
-    // Show first articles on first load
-loadMore();
-
-function loadMore() {
-    var hiddenArticles = $(document).find("article:hidden");
-    var i = 0;
-
-    if (i + additionalArticles >= hiddenArticles.length) {
-        loadMoreButton.style.display = "none";
-    }
-
-    while (i < hiddenArticles.length && i < additionalArticles) {
-        hiddenArticles[i].style.display = "block";
-        i++;
-    }
-}
-
-// Search Articles
