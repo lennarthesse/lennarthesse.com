847db2223fa2035375781bce1d9b2143b916440a
Lennart Hesse <lennart.hesse1301@gmail.com>
2023-05-11 10:36:23 +0200

    Finished imprint

--- src/routes/impressum/+page.svelte
+++ src/routes/impressum/+page.svelte
@@ -6,28 +6,32 @@
     <h1>Impressum</h1>
 
     <h2>Angaben gem. § 5 TMG</h2>
-    <p>Lennart Hesse Design & Development <br>
-        Potsdamer Straße 13 <br>
-        28211 Bremen <br>
+    <p>Lennart Hesse Design & Development<br>
+        Potsdamer Straße 13<br>
+        28211 Bremen<br>
         Deutschland </p>
-    <p>Verantwortlich für den Inhalt:<br>
-        Lennart Hesse</p>
 
     <h2>Kontakt</h2>
-    <p>Telefon: +49 176 39886008 <br>
-        E-Mail: info@lennarthesse.com <br>
+    <p>Telefon: +49 176 39886008<br>
+        E-Mail: info@lennarthesse.com<br>
         Web: www.lennarthesse.com</p>
 
     <!--
     <h2>Umsatzsteuer</h2>
-    <p>Umsatzsteuer-Identifikationsnummer gemäß § 27 a UStG: <br>
+    <p>Umsatzsteuer-Identifikationsnummer gemäß § 27 a UStG:<br>
         DE113112927</p>
     -->
+
+    <h2>Inhaltlich verantwortlich</h2>
+    <p>Lennart Hesse<br>
+        Potsdamer Straße 13<br>
+        28211 Bremen<br>
+        Deutschland </p>
 
     <h2>Streitschlichtung</h2>
     <p>Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit. Die Plattform finden Sie unter <a href="http://www.ec.europa.eu/consumers/odr/" target="_blank">https://ec.europa.eu/consumers/odr</a>. Wir sind weder bereit noch verpflichtet, an einem Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
 
     <h2>Urheberrecht</h2>
     <p>Die durch den Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.</p>
-    <p>Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</p>
+    <p>Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</p>
 </section>
