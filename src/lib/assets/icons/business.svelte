ab93614fa90eeb4d36b613b28d3cba84a103b79f
Lennart Hesse <lennart.hesse1301@gmail.com>
2023-05-22 15:08:44 +0200

    Added business icon

--- src/lib/assets/icons/business.svelte
+++ src/lib/assets/icons/business.svelte
@@ -1,6 +1,11 @@
 <svg viewBox="0 0 100 100"
-    font-size="70"
-    font-family="serif"
+    font-size="30"
+    font-family="comfortaa"
     fill="#333333">
-    <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle">Aa</text>
+    <rect fill="transparent" stroke="#333333" stroke-width="4%" x="2%" y="12%" width="96%" height="64%" rx="5%"/>
+    <rect x="30%" y="86%" width="40%" height="4%" rx="2%"/>
+    <path d="M 37,88 L 40,76 L 60,76 L 63,88 Z" />
+
+    <circle cx="50%" cy="44%" r="20%" />
+    <text fill="#ffffff" x="48%" y="48%" text-anchor="middle" dominant-baseline="middle">€</text>
 </svg>
